227058b8cb79c613d6b022f47761e08f866ca9e6
rickdevelop <riccardosalemi31@gmail.com>
2019-04-16 17:19:55 +0200

    fine

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var km = prompt('Inserisci il  numero  di kilometri da percorrere');
 var età = prompt('Inserisci la tua età');
 var prezzo = km * 0.21;
+if (età < 18) {
+prezzo = (prezzo - (prezzo / 100 * 20));
+} else if (età > 65) {
+prezzo = (prezzo - (prezzo / 100 * 40));
+} else {
+prezzo = prezzo;
+}
+document.write ('il prezzo è di:');
+document.writeln(prezzo + 'euro');
